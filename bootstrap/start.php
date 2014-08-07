8bb3a964009dae5e71e667f876007f8792e1b597
Kirill Balchenkov <kirill00k@gmail.com>
2014-08-07 14:41:08 -0400

    Updated start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,19 +24,12 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(function() {
+$env = $app->detectEnvironment(array(
 
-    # See if there's an environment.php file...
-    # If it exists, get environment string from there
-    if(file_exists(__DIR__.'/environment.php')) {
-        return require __DIR__.'/environment.php';
-    }
-    # If not found, assume production
-    else {
-        return 'production';
-    }
+    'local' => array('local'),
+    'production' => array('tunnel.pagodabox.com'),
 
-});
+));
 
 /*
 |--------------------------------------------------------------------------
